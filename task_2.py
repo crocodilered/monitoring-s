a933df4ef5d7f97c998ff68fe67ad05386b2c81a
crocodilered <serge.zolotukhin@gmail.com>
2020-08-23 12:03:39 +0300

    Refactor slightly
    Rename variables, put code to procedures, optimize iterations.

--- task_2.py
+++ task_2.py
@@ -2,10 +2,56 @@ import elasticsearch
 import matplotlib.pyplot as plt
 
 
+def show_csv(buckets):
+    csv_table = []
+
+    for x in buckets:
+        for y in x['dt_histogram']['buckets']:
+            csv_table.append((
+                y['key_as_string'][:10],
+                x['key'],
+                str(int(y['sum_bytes']['value']))
+            ))
+
+    csv_table.sort(key=lambda tup: (tup[0], tup[1]))
+
+    print(';'.join(['Date', 'Host', 'Bytes']))
+
+    for row in csv_table:
+        print(';'.join(row))
+
+
+def show_histogram(buckets):
+    fig, ax = plt.subplots()
+
+    dates, bottom, sums = [], [], {}
+
+    for i, x in enumerate(buckets):
+        host = x['key']
+        sums[host] = []
+
+        for y in x['dt_histogram']['buckets']:
+            sums[host].append(y['sum_bytes']['value'])
+
+            if i == 0:
+                dates.append(y['key_as_string'][8:10])
+                bottom.append(0)
+
+        ax.bar(dates, sums[host], 0.5, label=host)
+
+        for i, b in enumerate(sums[host]):
+            bottom[i] += b
+
+    ax.set_xlabel('Month days')
+    ax.set_ylabel('Bytes sums')
+
+    ax.legend()
+
+    plt.show()
+
+
 if __name__ == '__main__':
     es = elasticsearch.Elasticsearch()
-
-    # QUERY ############################################################################################################
 
     res = es.search(index='logs', body={
         'size': 0,
@@ -41,57 +87,5 @@ if __name__ == '__main__':
         }
     })
 
-    # CSV OUT ##########################################################################################################
-
-    table = []
-
-    for x in res['aggregations']['hosts']['buckets']:
-        for y in x['dt_histogram']['buckets']:
-            table.append((
-                y['key_as_string'][:10],
-                x['key'],
-                str(int(y['sum_bytes']['value']))
-            ))
-
-    table.sort(key=lambda tup: (tup[0], tup[1]))
-
-    print(';'.join(['Date', 'Host', 'Bytes']))
-
-    for x in table:
-        print(';'.join(x))
-
-    del table
-
-    # PLOT FUN #########################################################################################################
-
-    hosts_buckets = res['aggregations']['hosts']['buckets']
-
-    labels, bottom = [], []
-    for x in hosts_buckets[0]['dt_histogram']['buckets']:
-        labels.append(x['key_as_string'][8:10])
-        bottom.append(0)
-
-    hosts, means = [], {}
-
-    for x in hosts_buckets:
-        h = x['key']
-        hosts.append(h)
-        means[h] = []
-        for y in x['dt_histogram']['buckets']:
-            means[h].append(y['sum_bytes']['value'])
-
-    width = 0.4
-
-    fig, ax = plt.subplots()
-
-    for x in hosts:
-        ax.bar(labels, means[x], width, label=x)
-
-        for i, b in enumerate(means[x]):
-            bottom[i] += b
-
-    ax.set_xlabel('Dates')
-    ax.set_ylabel('Sums of bytes')
-    ax.legend()
-
-    plt.show()
+    show_csv(res['aggregations']['hosts']['buckets'])
+    show_histogram(res['aggregations']['hosts']['buckets'])
